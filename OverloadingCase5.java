43c450a315cfa3f97accf866bb71522acbce6c59
Amogh R Shukla <110371842+AmoghShukla@users.noreply.github.com>
2025-07-03 10:38:21 +0530

    Add files via upload

---
+++ OverloadingCase5.java
@@ -0,0 +1,28 @@
+package Java;
+
+class Animal{
+}
+
+class Monkey extends Animal{	
+}
+
+public class OverloadingCase5 {
+	public void m1(Animal a) {
+		System.out.println("Animal Version");
+	}
+	
+	public void m1(Monkey m) {
+		System.out.println("Monkey Version");
+	}
+
+	public static void main(String[] args) {
+		OverloadingCase5 oc5 = new OverloadingCase5();
+		Animal a = new Animal();
+		Monkey m = new Monkey();
+		oc5.m1(a); // Calls the Animal version
+		oc5.m1(m); // Calls the Monkey version
+		oc5.m1(null); // Calls the Animal version due to method resolution rules
+
+	}
+
+}
